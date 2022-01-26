44a748b293d7e7703650a9154d0dfc244d07e715
John Flaherty <jflaher4@nd.edu>
2022-01-26 18:32:48 -0500

    initial js

--- assignment3.js
+++ assignment3.js
@@ -1,7 +1,31 @@
-var container = document.getElementsByClassName('letter')[0];
-document.addEventListener('click', function(event) {
-	if (container !== event.target && !container.contains(event.target)) {
-		console.log('clicking outside the div');
-	}
-});
+let pos = 0;
+
+const keyboard = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D",
+					"F","G","H","J","K","L","Submit","Z","X","C","V","B",
+					"N","M","Del"]
+
+function addToBoard(letter) {
+	let square = document.getElementsByClassName("letter")[pos];
+	square.textContent = letter;
+	square.style.color = "black";
+	pos++;
 	
+}
+
+function keyPressed(i) {
+	console.log(i);
+	if (i != 19 && i != 27) {
+		addToBoard(keyboard[i]);
+	}
+}
+
+function keyboardListen() {
+	for (let i = 0; i < keyboard.length; i++) {
+		document.getElementsByClassName("key")[i].addEventListener("click",function() {
+			keyPressed(i);
+		});
+	}
+}
+
+keyboardListen();
+

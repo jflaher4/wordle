3672d506bc74f167135fc9c9964262fa3d5d80e0
John Flaherty <jflaher4@nd.edu>
2022-01-26 15:36:16 -0500

    changed to buttons

--- assignment3.js
+++ assignment3.js
@@ -1,1 +1,7 @@
-console.log('Hi');
+var container = document.getElementsByClassName('letter')[0];
+document.addEventListener('click', function(event) {
+	if (container !== event.target && !container.contains(event.target)) {
+		console.log('clicking outside the div');
+	}
+});
+	

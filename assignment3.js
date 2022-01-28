7705d300449711731de55d3cee173580a2b6a501
John Flaherty <jflaher4@nd.edu>
2022-01-27 19:11:00 -0500

    some updates

--- assignment3.js
+++ assignment3.js
@@ -6,6 +6,8 @@ const keyboard = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D",
 					"F","G","H","J","K","L","Submit","Z","X","C","V","B",
 					"N","M","Del"]
 
+const URL = 'https://raw.githubusercontent.com/JaneClelandHuang/Paradigms2022/main/data/common-words.txt';
+
 function addToBoard(letter) {
 	
 	let square = document.getElementsByClassName("letter")[pos];
@@ -15,11 +17,11 @@ function addToBoard(letter) {
 
 }
 
-function submitWord() {
-	console.log("word submitted");
+function clearBoard() {
+	console.log("clear");
 }
 
-function checkboxPressed() {
+function darkMode() {
 	
 	// Swicthes color variables
 	if (color1 === "black") {
@@ -27,17 +29,19 @@ function checkboxPressed() {
 		color2 = "black";
 		document.getElementById("circleback").style.filter="invert(100%)";
 		document.getElementById("github").style.filter="invert(100%)";
+		document.getElementById("dark").src = "sun.png";
+		document.getElementById("dark").style.filter = "invert(100%)";
 	} else {
 		color1 = "black";
 		color2 = "white";
 		document.getElementById("circleback").style.filter="invert(0%)";
 		document.getElementById("github").style.filter="invert(0%)";
+		document.getElementById("dark").src = "moon.png";
+		document.getElementById("dark").style.filter = "invert(0%)";
 	}
 	// Inverts colors of all elements
-		
 	document.body.style.background                            = color2;
 	document.getElementById("title").style.color              = color1;
-	document.getElementById("dark").labels[0].style.color     = color1;
 	document.getElementById("header").style.borderBottomColor = color1;	
 	// Inverts board with letters
 	for (let i = 0; i < 30; i++) {
@@ -73,28 +77,36 @@ function checkboxPressed() {
 	
 }
 
-function keyPressed(i) {
-	
-	if (i !== 19 && i !== 27) {
-		addToBoard(keyboard[i]);
+function keyPressed(key) {
+
+	if ((97 <= key && key <= 122) || (65 <= key && key <= 90)){
+		addToBoard(String.fromCharCode(key).toUpperCase());
+	} else if (key === 13) {
+		console.log("enter");
+	} else if (key === 8 || key === 46) {
+		console.log("delete");
 	}
-	
+}
+
+function keyClicked(i) {
+	if (i !== 19 && i !== 27) {
+		addToBoard(keyboard[i])
+	}
 }
 
 function eventListen() {
 	
-	document.getElementById('dark').addEventListener('change',function() {
-		checkboxPressed();
-	});
+	document.onkeydown = function(e) {
+		e = e || window.event;
+		keyPressed(e.keyCode);
+	};
 	
 	for (let i = 0; i < keyboard.length; i++) {
 		// Listen for click
 		document.getElementsByClassName("key")[i].addEventListener("click",function() {
-			keyPressed(i);
+			keyClicked(i);
 		});
 	}
 	
 }
-
-eventListen();
 
